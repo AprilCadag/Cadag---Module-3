3de679460969cc9b3b9c4c6c24b50631b36d9c23
AprilCadag <128223760+AprilCadag@users.noreply.github.com>
2024-05-15 00:09:16 +0800

    Update gimoToken.sol

--- gimoToken.sol
+++ gimoToken.sol
@@ -7,6 +7,34 @@ contract gimoToken is ERC20 {
 
   address private admin;
   
-  constructo(string memory tokenName, string memory tokenSymbol, uint256 totalToken)
+  constructor(string memory tokenName, string memory tokenSymbol, uint256 totalToken) ERC20(tokenName, tokenSymbol) {
+     _mint(msg.sender, totalToken);
+        admin = msg.sender;
+    }
 
-  
+  modifier onlyAdmin() {
+        require(msg.sender == admin, "Access restricted to admin");
+        _;
+    }
+
+  function generateTokens(address recipient, uint256 amount) external onlyAdmin {
+        _mint(recipient, amount);
+    }
+
+  function burnTokens(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+
+  function transfer(address recipient, uint256 amount) public override returns (bool) {
+        require(recipient != address(0), "Invalid recipient address");
+        _transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+  function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
+        require(recipient != address(0), "Invalid recipient address");
+        _transfer(sender, recipient, amount);
+        _approve(sender, msg.sender, allowance(sender, msg.sender) - amount);
+        return true;
+    }
+}
