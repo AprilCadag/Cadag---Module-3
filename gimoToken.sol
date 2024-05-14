c52e413e35599a233b5d69f2798b8293e740a379
AprilCadag <128223760+AprilCadag@users.noreply.github.com>
2024-05-15 00:07:01 +0800

    Create gimoToken.sol

---
+++ gimoToken.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract gimoToken is ERC20 {
+
+  address private admin;
+  
+  constructo(string memory tokenName, string memory tokenSymbol, uint256 totalToken)
+
+  
